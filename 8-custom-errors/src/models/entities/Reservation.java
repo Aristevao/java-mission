d16218ccb06c83e531c7a72e7f3adcb9b042c39c
Aristevao <aristevao@hotmail.com>
2021-12-14 22:07:45 -0300

    [T-09] Bad solution (returns string)

--- 8-custom-errors/src/models/entities/Reservation.java
+++ 8-custom-errors/src/models/entities/Reservation.java
@@ -18,13 +18,21 @@ public class Reservation {
     }
 
     Long duration() {
-        long diff =  checkOut.getTime() - checkIn.getTime(); // difference between both in milliseconds
+        long diff = checkOut.getTime() - checkIn.getTime(); // difference between both in milliseconds
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public void updateDates(Date checkIn, Date checkOut) {
+    public String updateDates(Date checkIn, Date checkOut) {
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)) {
+            return "Error in reservation: Reservation dates for update must be future dates";
+        }
+        if (!checkOut.after(checkIn)) {
+            return "Error in reservation: Check-out date must be after check-in date";
+        }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+        return null;
     }
 
     @Override
